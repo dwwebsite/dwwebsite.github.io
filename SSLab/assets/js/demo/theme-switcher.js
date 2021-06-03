c9c6ffb69e30be0d42c96ff22945c7aea7aed9f6
dwwebsite <profdongwang@gmail.com>
2021-06-03 09:42:54 -0500

    123

---
+++ SSLab/assets/js/demo/theme-switcher.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    /* ======= Style Switcher ======= */   
+    $('#config-trigger').on('click', function(e) {
+
+        var $panel = $('#config-panel');
+        var panelVisible = $('#config-panel').is(':visible');
+        if (panelVisible) {
+            $panel.hide();          
+        } else {
+            $panel.show();
+        }
+        e.preventDefault();
+
+    });
+    
+    $('#config-close').on('click', function(e) {
+        e.preventDefault();
+        $('#config-panel').hide();
+    });
+    
+    
+    
+    $('#color-options a').on('click', function(e) { 
+        var $styleSheet = $(this).attr('data-style');
+        var $logoImage = $(this).attr('data-logo');
+		$('#theme-style').attr('href', $styleSheet);
+		$('#logo').attr('src', $logoImage);		
+				
+		var $listItem = $(this).closest('li');
+		$listItem.addClass('active');
+		$listItem.siblings().removeClass('active');
+		
+		e.preventDefault();
+		
+	});
+});
